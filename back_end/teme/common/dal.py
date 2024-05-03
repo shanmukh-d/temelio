db2f06a7d4b6f938c2bbd6e396615008ca3c56c9
Shanmukh Dundigalla <dundigallashanmukh@gmail.com>
2024-05-03 18:44:42 +0530

    Added comment to check github workflow

--- back_end/teme/common/dal.py
+++ back_end/teme/common/dal.py
@@ -5,4 +5,5 @@ def get_user_from_username(username: str) -> User:
     return User.objects.filter(username=username).first()
 
 def check_if_user_has_permission(user: User, permission_name: str) -> bool:
+    "checks if the user has the permission or not"
     return UserPermissions.objects.filter(user=user, permissions__name=permission_name).exists()
